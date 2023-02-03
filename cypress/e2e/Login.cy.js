61e84a2909186beea29b8c2b6583ae539104c2bc
ZaraGevorgian <tovmasyan@sigma-d.ru>
2023-02-03 20:02:44 +0400

    finished

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,13 +1,10 @@
-describe('Verify login functionality', () => {
-    it('login', () => {
-        cy.visit (Cypress.env('globalUrl'))
-        cy.url().should('include', Cypress.env('globalUrl'))
-        cy.get(':nth-child(1) > .form-control').type('tovmasyan@sigma-d.ru')
-        cy.get(':nth-child(2) > .form-control').type('test_passs')
-        cy.get('.signup').eq(1).click();
-        cy.get('.map').should("be.visible");
-
-
-
-    })
-  })
+describe("Verify login functionality", () => {
+  it("login", () => {
+    cy.visit(Cypress.env("globalUrl"));
+    cy.url().should("include", Cypress.env("globalUrl"));
+    cy.get(":nth-child(1) > .form-control").type("tovmasyan@sigma-d.ru");
+    cy.get(":nth-child(2) > .form-control").type("test_passs");
+    cy.get(".signup").eq(1).click();
+    cy.get(".map").should("be.visible");
+  });
+});
